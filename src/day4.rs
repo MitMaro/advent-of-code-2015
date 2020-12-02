36d0abaf3bbf5328fb74ded06e4e73e1880f8f14
Tim Oram <dev@mitmaro.ca>
2020-12-02 17:53:42 -0330

    Add day 4 solution

---
+++ src/day4.rs
@@ -0,0 +1,33 @@
+use md5::{Digest, Md5};
+
+#[aoc(day4, part1)]
+pub fn part1(key: &str) -> usize {
+	let mut i = 0;
+
+	loop {
+		let mut hasher = Md5::new();
+		hasher.update(format!("{}{}", key, i));
+		let result = hasher.finalize();
+
+		if result[0] == 0 && result[1] == 0 && result[2] < 0x10 {
+			return i;
+		}
+		i += 1;
+	}
+}
+
+#[aoc(day4, part2)]
+pub fn part2(key: &str) -> usize {
+	let mut i = 0;
+
+	loop {
+		let mut hasher = Md5::new();
+		hasher.update(format!("{}{}", key, i));
+		let result = hasher.finalize();
+
+		if result[0] == 0 && result[1] == 0 && result[2] == 0 {
+			return i;
+		}
+		i += 1;
+	}
+}
